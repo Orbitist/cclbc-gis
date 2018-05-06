33d0b1b1cf742eea1e857115118b98160b6e11d5
Nicholas Gunner <support@getbeyondwords.com>
2018-05-06 14:48:58 -0600

    added polygons

--- js/scripts.js
+++ js/scripts.js
@@ -6,47 +6,16 @@ var map = new mapboxgl.Map({
     zoom: 9,
     minZoom: 9
 });
+
 map.on('load', function () {
-    map.addSource('foreclosures', {
-        'type': 'vector',
-        'url': 'cclb.cjgv6qfpn024sbcln75x1bn13-6o38r'
-    });
+
     map.addLayer({
-    'id': 'foreclosures',
-    'source': 'foreclosures',
-    'source-layer': 'foreclosures',
-    'type': 'fill',
-    'paint': {
-        'fill-color': {
-            property: "Sale_Price",
-            stops: [
-              [4999, '#000000'],
-              [5000, '#38e54a'],
-              [100000, '#ff1f1f'],
-              [500, '#000000']
-            ]
+        "id": "foreclosures",
+        "type": "fill",
+        "source": {
+            type: 'vector',
+            url: 'mapbox://cclb.cjgv6qfpn024sbcln75x1bn13-6o38r'
         },
-        'fill-outline-color': '#ffffff',
-        'fill-opacity': 0.7
-      }
-    }, 'water');
-  });
-// When a click event occurs near a polygon, open a popup at the location of
-// the feature, with description HTML from its properties.
-map.on('click', function (e) {
-    var features = map.queryRenderedFeatures(e.point, { layers: ['foreclosures'] });
-    if (!features.length) {
-        return;
-    }
-    var feature = features[0];
-    var popup = new mapboxgl.Popup()
-        .setLngLat(map.unproject(e.point))
-        .setHTML('$' + feature.properties["Sale_Price"])
-        .addTo(map);
-});
-// Use the same approach as above to indicate that the symbols are clickable
-// by changing the cursor style to 'pointer'.
-map.on('mousemove', function (e) {
-    var features = map.queryRenderedFeatures(e.point, { layers: ['foreclosures'] });
-    map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+        "source-layer": "2018_foreclosures"
+    });
 });
